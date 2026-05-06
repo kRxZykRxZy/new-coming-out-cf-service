67a95e0d840904df64d998f45c5f5a86d7304461
dsfg fgd <no@thankyou.com>
2026-05-06 19:41:40 +0100

    implement cloudcasts

---
+++ models/cloudcasts/newCloudcast.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const { Cloudcast } = require('../../db/models');
+
+async function createNewCloudcast(domain,  sessionToken) {
--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -2,12 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const loginRoutes = require('../routes/auth/login.js');
 const signupRoutes = require('../routes/auth/signup.js');
+const cookieParser = require('cookie-parser');
+const sessionRoutes = require('../routes/auth/sessions.js');
+const logoutRoutes = require('../routes/auth/logout.js');
+const newCloudcastRoutes = require('../routes/cloudcasts/newCloudcast.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', loginRoutes);
 app.use('/api/auth', signupRoutes);
+app.use('/api/auth', sessionRoutes);
+app.use('/api/auth', logoutRoutes);
+app.use('/api/cloudcasts', newCloudcastRoutes);
 
 module.exports = app;
